d9d2e99a9a34ad495875181db2902489fd607476
ePEa1 <zmzm5442@gmail.com>
2022-05-03 16:06:28 +0900

    Title Scene
    타이틀 씬 만드는중

---
+++ Assets/Scripts/UI/LanguageSetter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LanguageSetter : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _text;
+    [SerializeField] LanguageData _data;
+
+    int _nowLanguage;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
